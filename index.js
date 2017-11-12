ced511e67ca04ed9abd43455f70e453e54035bd0
Cinaly <834518573@qq.com>
2017-11-12 16:40:05 +0800

    ..

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by web-01 on 2017/11/12.
+ */
+const express=require('express');
+
+let app=express();
+
+app.get('/',(req,res)=>{
+   // res.send("hello,expressss");
+
+
+});
+
+app.listen(80);
