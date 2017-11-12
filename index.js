d5cdc001625d260aff251acf616d063f1af26316
Cinaly <834518573@qq.com>
2017-11-12 17:06:37 +0800

    ..

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
  * Created by web-01 on 2017/11/12.
  */
 const express=require('express');
+const bodyParser=require('body-parser');
 
 let app=express();
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
 
 app.get('/',(req,res)=>{
    // res.send("hello,expressss");
-
+    res.sendfile("./public/index.html","",(err,res)=>{
+        console.log('....',res);
+    });
 
 });
 
+app.post('/signIn',(req,res)=>{
+    let username=req.body.username;
+    let password=req.body.password;
+    res.send(username+":"+password);
+});
 app.listen(80);
